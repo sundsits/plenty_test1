eeba79db5ffe1df15c89c830dffee189e1ec0058
Andi-SundSITS <31002514+Andi-SundSITS@users.noreply.github.com>
2018-05-08 10:13:26 +0200

    test

--- resources/lib/ftpclient_connector.php
+++ resources/lib/ftpclient_connector.php
@@ -7,13 +7,15 @@ $ftp = new \FtpClient\FtpClient();
 $ftp->connect($host);
 $ftp->login($login, $password);
 
+return json_encode($ftp);
+
 // count in the current directory
 $total = $ftp->count();
 
 // scan the current directory and returns the details of each item
 $items = $ftp->scanDir();
 
-return $total;
+//return $total;
 
 // scan the current directory (recursive) and returns the details of each item
 //return ($ftp->scanDir('.', true));
